bcd767d8f5bb54ad90b3a6883fb1f19e8a37c07c
himal9626 <himal9629@gmail.com>
2025-01-02 10:40:18 +0530

    adding gpai as condition

--- src/common/downloadOps.ts
+++ src/common/downloadOps.ts
@@ -1,61 +1,69 @@
-import fs from "fs";
-import path from "path";
+import fs from 'fs'
+import path from 'path'
 
-import { handleFailure } from "../services/api-actions";
-import { executeCommand } from "./executables";
-import { DownloadModelTypes } from "./types";
-import { setConfigs } from "./setConfigs";
-import { settingStore } from "../store";
+import { handleFailure } from '../services/api-actions'
+import { settingStore } from '../store'
+import { executeCommand } from './executables'
+import { DownloadModelTypes } from './types'
 
-export const downloadModel = async ({ url, pipeline }: DownloadModelTypes) => {
-  const filename = path.basename(url);
-  const fileExtension = path.extname(filename);
-  const staticFilename = `model${fileExtension}`;
-  let targetFolder: string;
-  switch (pipeline) {
-    case "image":
-      targetFolder = "./Results/Model";
-      break;
-    default:
-      targetFolder = "./results/model_paths";
-  }
+export const downloadModel = async ({
+    url,
+    pipeline,
+    app
+}: DownloadModelTypes) => {
+    const filename = path.basename(url)
+    const fileExtension = path.extname(filename)
+    const staticFilename = `model${fileExtension}`
+    let targetFolder: string
 
-  try {
-    if (!fs.existsSync(targetFolder)) {
-      fs.mkdirSync(targetFolder, { recursive: true });
+    if (app === 'saas') {
+        switch (pipeline) {
+            case 'image':
+                targetFolder = './Results/Model'
+                break
+            default:
+                targetFolder = './results/model_paths'
+        }
+    } else {
+        targetFolder = './results/model_paths'
     }
 
-    await executeCommand(
-      `curl -s ${url} --output ${targetFolder}/${staticFilename}`
-    );
-    console.log("model downloaded");
-  } catch (error) {
-    console.error("Error in downloadAndMoveModel:", error);
-    await handleFailure({
-      reason: `Error in downloadAndMoveModel: ${error}`,
-    });
-  }
-};
+    try {
+        if (!fs.existsSync(targetFolder)) {
+            fs.mkdirSync(targetFolder, { recursive: true })
+        }
+
+        await executeCommand(
+            `curl -s ${url} --output ${targetFolder}/${staticFilename}`
+        )
+        console.log('model downloaded')
+    } catch (error) {
+        console.error('Error in downloadAndMoveModel:', error)
+        await handleFailure({
+            reason: `Error in downloadAndMoveModel: ${error}`
+        })
+    }
+}
 
 export const downloadDataset = async ({ url }: { url: string }) => {
-  const filename = path.basename(url);
-  const fileExtension = path.extname(filename);
-  const staticFilename = `dataset${fileExtension}`;
-  const targetFolder = "./datasets";
+    const filename = path.basename(url)
+    const fileExtension = path.extname(filename)
+    const staticFilename = `dataset${fileExtension}`
+    const targetFolder = './datasets'
 
-  try {
-    settingStore({ fileName: staticFilename });
-    if (!fs.existsSync(targetFolder)) {
-      fs.mkdirSync(targetFolder, { recursive: true });
+    try {
+        settingStore({ fileName: staticFilename })
+        if (!fs.existsSync(targetFolder)) {
+            fs.mkdirSync(targetFolder, { recursive: true })
+        }
+        await executeCommand(
+            `curl -s ${url} --output ${targetFolder}/${staticFilename}`
+        )
+        console.log('dataset downloaded')
+    } catch (error) {
+        console.error('Error on Downloading Datasets', error)
+        await handleFailure({
+            reason: `Error in Downloading : ${error}`
+        })
     }
-    await executeCommand(
-      `curl -s ${url} --output ${targetFolder}/${staticFilename}`
-    );
-    console.log("dataset downloaded");
-  } catch (error) {
-    console.error("Error on Downloading Datasets", error);
-    await handleFailure({
-      reason: `Error in Downloading : ${error}`,
-    });
-  }
-};
+}
